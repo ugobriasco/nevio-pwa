0b0dd6c62d23b2e1470ea2ac5098494dd0b47a55
Ugo <ugo.briasco@sumup.com>
2020-09-10 12:37:12 +0200

    JS Renders links in the sidenav

--- src/js/lib/nav-helper.js
+++ src/js/lib/nav-helper.js
@@ -1,11 +1,31 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
+const openNav = () => {
   document.getElementById('mySidebar').style.width = '60%';
+  populateMenu();
   //document.getElementById('page-wrapper').style.marginLeft = '250px';
-}
+};
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
+const closeNav = () => {
   document.getElementById('mySidebar').style.width = '0';
   //document.getElementById('page-wrapper').style.marginLeft = '0';
-}
+};
+
+// Populate links in the sidebar
+const populateMenu = () => {
+  const list = [
+    { label: 'Media', link: '/' },
+    { label: 'Sotries', link: './discover.html' }
+  ];
+
+  const sidenav = document.getElementById('sidenav-items');
+  sidenav.innerHTML = '';
+
+  list.forEach((item) => {
+    let a = document.createElement('a');
+    console.log(item);
+    a.href = item.link;
+    a.innerHTML = item.label;
+    sidenav.append(a);
+  });
+};
