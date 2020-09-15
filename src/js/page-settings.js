92d05326955b76f95d2ca79372cf9958e6b20246
Ugo <ugo.briasco@sumup.com>
2020-09-15 16:34:38 +0200

    CRU language selection in local storage

--- src/js/page-settings.js
+++ src/js/page-settings.js
@@ -3,50 +3,74 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const renderLanguageSelection = () => {
-  const languageAvailable = ['Default'].concat(Locale.getAvailable());
-  console.log(languageAvailable);
+  const { locale, languageName } = getTranslations();
 
   const container = document.getElementById('language-selection');
-  console.log(container);
-
-  const h2 = document.createElement('h2');
-  h2.innerHTML = 'Language';
-  container.append(h2);
-
-  /*
-  <div id="sidenav-language-select" class="custom-select" onClick="SideNav.toggleDropDown()">
-      <div class="custom-select__trigger"><span>Tesla</span>
-          <div class="arrow"></div>
-      </div>
-      <div class="custom-options" >
-          <span class="custom-option selected" data-value="tesla">Tesla</span>
-          <span class="custom-option" data-value="volvo">Volvo</span>
-          <span class="custom-option" data-value="mercedes">Mercedes</span>
-      </div>
-  </div>
-
-  */
+  container.innerHTML = '';
 
   const box = document.createElement('div');
   box.id = 'sidenav-language-select';
   box.classList.add('custom-select');
   box.setAttribute('onClick', 'toggleDropDown()');
-  box.innerHTML = `<div class="custom-select__trigger"><span>${languageAvailable[0]}</span>
+  box.innerHTML = `<div class="custom-select__trigger"><span id="txt-languageName">${languageName}</span>
       <div class="arrow"></div>
   </div>`;
 
   const select = document.createElement('div');
   select.classList.add('custom-options');
-  languageAvailable.forEach((language, i) => {
-    const span = document.createElement('span');
-    span.classList.add('custom-option');
-    span.setAttribute('data-value', language);
-    span.innerHTML = language;
-    select.append(span);
-  });
+  select.id = 'custom-options';
 
   box.append(select);
   container.append(box);
+
+  renderLanguageOptions(locale);
+};
+
+const renderLanguageOptions = (locale) => {
+  const currentLocale = locale == 'null' ? null : locale;
+
+  const select = document.getElementById('custom-options');
+  select.innerHTML = '';
+
+  const languageAvailable = [{ key: null, languageName: 'Default' }].concat(
+    Locale.getAvailable()
+  );
+
+  languageAvailable.forEach((item, i) => {
+    const span = document.createElement('span');
+    span.classList.add('custom-option');
+    span.id = `opt-${item.key}`;
+
+    if (item.key == currentLocale) {
+      span.classList.add('selected');
+    }
+    span.setAttribute('data-value', item.key);
+    span.setAttribute('onClick', `selectLanguage('${item.key}')`);
+    span.innerHTML = item.languageName;
+    select.append(span);
+  });
+};
+
+const getTranslations = () => {
+  const locale = Locale.get();
+  const translations = Locale.getTranslations(locale);
+  const languageName =
+    translations && translations.languageName
+      ? translations.languageName
+      : 'Default';
+  return { locale, languageName };
+};
+
+const updateTranslations = () => {
+  document.getElementById(
+    'txt-languageName'
+  ).innerHTML = getTranslations().languageName;
+};
+
+const selectLanguage = (locale) => {
+  Locale.set(locale);
+  updateTranslations();
+  renderLanguageOptions(locale);
 };
 
 const toggleDropDown = () => {
